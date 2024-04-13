7d53926102eb7917edfc89b9556db902f8056658
Abid Dar <abiddar1979@gmail.com>
2024-04-13 15:47:47 +0500

    files added

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+/* Task # 11 : Names: Store the names of a few of your friends in a Array called names . Print ewch person's Name by accessing each element ib the list, one at a time.
+
+Task # 10 (part 2)  : Adding Comments: Choose two of the programs you have written ,and add at least one comment to each . If you dont have anything spacific to write because your program are too simple at this point , just add your name and the current date at the top of each program file . Then write one sentence discribing what the program dose.*/
+// ABID DAR
+//thursday 19-2-2024
+//Storing friends name in an Array
+let names = ["Sabtain", "Sufian", "Cobra", "Kamran"];
+//printing names one by one
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
